f7603b1df0215e9040ea7e848a1a16a7e498c99f
Ameya Patil <144146919+Ameyapatil28@users.noreply.github.com>
2025-05-12 23:34:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open instagram" or "open insta" in c.lower():
+        webbrowser.open("https://instagram.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    
+   
+
+
+if __name__ == "__main__":
+    speak("Initilizing Shadow......")
+    while True:
+        #listen for wake word 'Shadow'
+        #obtain audio from microphone
+        r = sr.Recognizer()
+       
+    
+    #recognize speech
+        print("recognizing..")
+        try:
+            with sr.Microphone() as source:
+                print("Listening.....")
+                audio = r.listen(source,timeout=10, phrase_time_limit=10)
+            word = r.recognize_google(audio)
+            if(word.lower() == "hello"):
+                speak("Yes")
+                print("yes")
+                #listen for command
+                with sr.Microphone() as source: 
+                    print("Shadow Activated.....")
+                    audio = r.listen(source,timeout=10, phrase_time_limit=10)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+
+
+        except Exception as e:
+            print("error; {0}".format(e))
